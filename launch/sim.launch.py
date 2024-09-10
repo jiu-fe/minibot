488d686d49591845ecf53c82d72d82ae882fc084
Yong Jun <tyjun0528@gmail.com>
2024-09-11 01:21:02 +0800

    Implemented camera in simulation
    	- Created URDF for camera and set up the sensor in gazebo simulation.
    	- Implemented image transport republisher in simulation launch file (topic: /camera/image_raw/compressed).

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -8,13 +8,26 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
+# Image Transport Republishers
+# terminal command example: ros2 run image_transport republish raw compressed --ros-args -r in:=/camera/image_raw -r out/compressed:=/camera/image_raw/compressed
+def image_transport_republisher(transport):
+    return Node(
+        package='image_transport',
+        executable='republish',
+        name=f'image_transport_republish_{transport}',
+        arguments=['raw', transport],
+        remappings=[
+            ('in', '/camera/image_raw'),
+            (f'out/{transport}', f'/camera/image_raw/{transport}'),
+        ],
+    )
+
 def generate_launch_description():
 
     package_name= 'minibot'
 
     # Update the path to your custom world file
-    # Remember to remove <uri> in the sdf file
-    world_file_path = '/home/yj/ros2_ws/install/minibot/share/minibot/worlds/playground.sdf'
+    world_file_path = os.path.join(get_package_share_directory(package_name), 'worlds', 'playground.sdf')
     rviz_config_file_dir = os.path.join(get_package_share_directory(package_name), 'config', 'gz_sim_config.rviz')
 
     # robot_state_publisher node
@@ -36,9 +49,15 @@ def generate_launch_description():
             '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',  
             '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
             '/scan/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked',
+            '/camera/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
+            '/camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
         ],
         output='screen'
     )
+
+    # Execute Image Transport Republishers
+    image_transports = ['compressed','compressedDepth', 'theora', 'zstd' ]  # Add or remove formats as needed
+    image_republishers = [image_transport_republisher(transport) for transport in image_transports]
 
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -60,13 +79,19 @@ def generate_launch_description():
         parameters=[{'use_sim_time': True}],
         output='screen'
     )
-    # Launch!
-    return LaunchDescription([
-        rsp,
-        bridge,
-        GroupAction([
-            gazebo,
-            spawn_model,
-        ]),
-        rviz2
-    ])
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Add the nodes to the launch description
+    ld.add_action(rsp)
+    ld.add_action(bridge)
+    for republisher in image_republishers:
+        ld.add_action(republisher)
+    ld.add_action(GroupAction([gazebo, spawn_model]))
+    ld.add_action(rviz2)
+
+    # Generate the launch description and 
+    return ld
+
+    
