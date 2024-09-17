d524401243137b89e893f9445069930fe54b395e
Yong Jun <tyjun0528@gmail.com>
2024-09-18 01:34:43 +0800

    Implemented gz_ros2_control for differential drive. Implemented a basic velocity_publisher for the controller. Merge robot state publisher node into the sim.launch.py. deactivated gazebo_control (script still included but commented out).

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -3,10 +3,13 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, GroupAction
+from launch.actions import IncludeLaunchDescription, GroupAction, RegisterEventHandler, DeclareLaunchArgument
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import LaunchConfiguration, Command
+
 
 # Image Transport Republishers
 # terminal command example: ros2 run image_transport republish raw compressed --ros-args -r in:=/camera/image_raw -r out/compressed:=/camera/image_raw/compressed
@@ -28,25 +31,70 @@ def generate_launch_description():
 
     package_name= 'minibot'
 
-    # Update the path to your custom world file
-    world_file_path = os.path.join(get_package_share_directory(package_name), 'worlds', 'playground.sdf')
-    rviz_config_file_dir = os.path.join(get_package_share_directory(package_name), 'config', 'gz_sim_config.rviz')
+    # Declare the path to files
+    robot_description_xacro_file = os.path.join(
+        get_package_share_directory(package_name),
+        'description',
+        'robot.urdf.xacro'
+    )
+    world_file_path = os.path.join(
+        get_package_share_directory(package_name), 
+        'worlds', 
+        'playground.sdf'
+    )
+    rviz_config_file_dir = os.path.join(
+        get_package_share_directory(package_name), 
+        'config', 
+        'gz_sim_config.rviz'
+    )
+    robot_controllers = os.path.join(
+        get_package_share_directory(package_name), 
+        'config', 
+        'controller.yaml'
+    )
+    gazebo_params_file = os.path.join(
+        get_package_share_directory(package_name), 
+        'config', 
+        'gz_params.yaml'
+    )
+
+    # robot_state_publisher setup
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
+    
+    robot_description_config = Command ([
+        'xacro ', 
+        robot_description_xacro_file, 
+        ' use_ros2_control:=', 
+        use_ros2_control
+        ])
+    
+    params = {
+        'robot_description': ParameterValue(robot_description_config, value_type=str), 
+        'use_sim_time': use_sim_time
+    }
 
     # robot_state_publisher node
-    rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
     )
     
-    # Gazebo bridge
+    # gz_bridge node
+    # format: '<topic_name>@/[/]<ros2_topic_type_name>@/[/]<gazebo_topic_type_name>',
+    # ros2 topic type <topic_name> to check topi type_name in ros2
+    # gz topic -t <topic_name> -i to check topic type_name in gazebo
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=[
-
-            # Diffdrive
+            
+            # General
             '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+
+            # Gazebo_Control
             '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
             '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
             '/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model',
@@ -69,43 +117,105 @@ def generate_launch_description():
         output='screen'
     )
 
-    # Execute Image Transport Republishers
+    # Image Transport Republishers Node
     camera = 'image_raw'
     depth_camera = 'depth/image_raw'
     image_transports = ['compressed','compressedDepth', 'theora', 'zstd' ]  
-    image_republishers = [image_transport_republisher(transport, depth_camera) for transport in image_transports]
+    image_republishers = [image_transport_republisher(transport, depth_camera) 
+                          for transport in image_transports]
 
+    # gz launch world
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'),'launch','gz_sim.launch.py'
-                )]), launch_arguments={'gz_args': f'-r {world_file_path}',}.items()
+                    get_package_share_directory('ros_gz_sim'),
+                    'launch',
+                    'gz_sim.launch.py'
+                )]), 
+                launch_arguments=
+                {'gz_args': f'-r -v 4 {world_file_path}',
+                 'extra_gazebo_args': '--ros-args --params-file' + gazebo_params_file}.items()
     )
 
-    spawn_model = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'),'launch','gz_spawn_model.launch.py'
-                )]), launch_arguments={'topic': 'robot_description', 'name': 'minibot', 'z': '0.1'}.items()
+    # gz spawn robot entity 
+    gz_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-topic', 'robot_description', 
+                '-name', 'minibot',
+                '-allow_renaming', 'true',
+                '-z', '0.1'],
     )
 
+    # rviz2 node
     rviz2 = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         arguments=['-d', rviz_config_file_dir],
         parameters=[{'use_sim_time': True}],
-        output='screen'
+        output='both'
+    )
+
+    # controller spawn 
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+    
+    diff_drive_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller", 
+                   "--param-file", 
+                   robot_controllers],
     )
 
     # Create the launch description and populate
     ld = LaunchDescription()
 
     # Add the nodes to the launch description
-    ld.add_action(rsp)
+    ld.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=gz_spawn_entity,
+                on_exit=[joint_state_broadcaster_spawner],
+            )
+        ),
+    )
+
+    ld.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[diff_drive_controller_spawner],
+            )
+        ),
+    )
+
+    ld.add_action(
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='If true, use simulated clock'),
+    )
+
+    ld.add_action(
+        DeclareLaunchArgument(
+            'use_ros2_control',
+            default_value='true',
+            description='If true, use ros2_control'),
+    )
+
+    ld.add_action(node_robot_state_publisher)
     ld.add_action(bridge)
     for republisher in image_republishers:
         ld.add_action(republisher)
-    ld.add_action(GroupAction([gazebo, spawn_model]))
+    ld.add_action(GroupAction([gazebo, gz_spawn_entity]))
     ld.add_action(rviz2)
+    
+   
 
     # Generate the launch description and 
     return ld
